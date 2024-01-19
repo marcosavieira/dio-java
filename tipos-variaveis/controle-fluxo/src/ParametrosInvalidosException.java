88e43f6959cad654f67a6feee753551e2bb1f4dd
devmarcosav@gmail.com <devmarcosav@gmail.com>
2024-01-19 17:37:16 -0300

    feat: resolvendo desafio proposto

---
+++ tipos-variaveis/controle-fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+public class ParametrosInvalidosException extends Exception {
+    // Construtor que aceita uma mensagem de erro
+    public ParametrosInvalidosException(String mensagem) {
+        // Chame o construtor da superclasse (Exception) com a mensagem
+        super(mensagem);
+    }
+
+}
